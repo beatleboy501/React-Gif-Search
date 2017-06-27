c2b336986b0d83cce392b6580d4fdb278df14861
Andrew Allison <beatleboy501@gmail.com>
2017-06-27 18:25:36 -0400

    adding private and public routes and stubbing for connection with firebase

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { ConnectedRouter } from 'react-router-redux';
-import { Route } from 'react-router-dom';
-import { history } from './../store/configureStore';
+import {ConnectedRouter} from 'react-router-redux';
+import {Route, Redirect} from 'react-router-dom';
+import {history} from './../store/configureStore';
+import {connect} from 'react-redux';
 
 import Header from '../containers/Header';
 import Home from '../containers/Home';
@@ -9,21 +10,49 @@ import Signup from '../containers/Signup';
 import Login from '../containers/Login';
 import Favorites from '../containers/Favorites';
 
-export default class App extends React.Component {
-  render() {
-    return(
-      <ConnectedRouter history={ history }>
-        <div>
-          <Header/>
+const PrivateRoute = ({component: Component, authenticated, ...props}) => {
+  return (
+      <Route
+          {...props}
+          render={(props) => (authenticated === true)
+              ? <Component {...props} />
+              : <Redirect to={{pathname: '/login', state: {from: props.location}}} />}
+      />
+  )
+};
 
-          <div className="container">
-            <Route exact path="/" component={ Home } />
-            <Route exact path="/signup" component={ Signup } />
-            <Route exact path="/login" component={ Login } />
-            <Route exact path="/favorites" component={ Favorites } />
+const PublicRoute = ({component: Component, authenticated, ...props}) => {
+  return (
+      <Route
+          {...props}
+          render={(props) => authenticated === false
+              ? <Component {...props} />
+              : <Redirect to='/favorites' />}
+      />
+  );
+};
+
+class App extends React.Component {
+  render() {
+    return (
+        <ConnectedRouter history={ history }>
+          <div>
+            <Header/>
+
+            <div className="container">
+              <Route exact path="/" component={ Home }/>
+              <PublicRoute authenticated={this.props.authenticated }  path="/signup" component={ Signup } />
+              <PublicRoute authenticated={this.props.authenticated }  path="/login" component={ Login } />
+              <PrivateRoute authenticated={this.props.authenticated }  path="/favorites" component={ Favorites } />
+            </div>
           </div>
-        </div>
-      </ConnectedRouter>
+        </ConnectedRouter>
     );
   }
 }
+
+const mapStateToProps = (state) => {
+  return { authenticated: state.auth.authenticated };
+};
+
+export default connect(mapStateToProps)(App);
